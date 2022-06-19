7489be86a636826b85edb3c51a0f624459bc3308
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2022-06-19 13:22:44 -0400

    Move message files into message package; add unit tests

--- src/main/scala/info/ditrapani/overview2/message/message.scala
+++ src/main/scala/info/ditrapani/overview2/message/message.scala
@@ -1,4 +1,4 @@
-package info.ditrapani.overview2.line
+package info.ditrapani.overview2.message
 
 enum Color:
   case Blue
@@ -6,28 +6,31 @@ enum Color:
   case Red
   case Yellow
 
-def colorize(message: String, color: Color): String =
+def colorize(str: String, color: Color): String =
   val code = color match
     case Color.Blue => "94"
     case Color.Green => "32"
     case Color.Red => "31"
     case Color.Yellow => "33"
-  s"\u001B[${code}m${message}\u001B[0m"
+  s"\u001B[${code}m${str}\u001B[0m"
 
-case class ColoredString(message: String, color: Color)
+case class ColoredString(str: String, color: Color)
 
 type Message = String | ColoredString
 type Line = List[Message]
 
+def singleLine(coloredString: ColoredString): List[Line] =
+  List(List(coloredString))
+
 def display(lines: List[Line]): Unit =
   lines.foreach { displayLine(_) }
 
-private def lineToString(line: Line): String =
+private[message] def lineToString(line: Line): String =
   line
     .map { message =>
       message match
         case s: String => s
-        case ColoredString(message, color) => colorize(message, color)
+        case ColoredString(str, color) => colorize(str, color)
     }
     .mkString(" ")
 
---
+++ src/test/scala/info/ditrapani/overview2/message/messageSpec.scala
@@ -0,0 +1,53 @@
+package info.ditrapani.overview2.message
+
+import info.ditrapani.overview.Spec
+
+class MessageSpec extends Spec:
+  "singleLine" - {
+    "constructs a List[Line] frome a SoloredString" in {
+      val cString = ColoredString(
+        """well...
+        hello there""",
+        Color.Yellow
+      )
+      val lines = singleLine(cString)
+      lines shouldBe List(List(cString))
+      println("visual test:")
+      display(singleLine(cString))
+      println("end visual test")
+      // This is a visual test.  You must look at the test output.
+    }
+  }
+
+  "lineToString" - {
+    "converts a single line into a colorized String ready to be printed" in {
+      val line = List[Message](
+        "default1",
+        ColoredString("red", Color.Red),
+        ColoredString("blue", Color.Blue),
+      )
+      lineToString(line).shouldBe(
+        "default1 \u001B[31mred\u001B[0m \u001B[94mblue\u001B[0m"
+      )
+    }
+  }
+
+  "display" - {
+    "colors and prints out the messages" in {
+      val line1 = List[Message](
+        "default1",
+        ColoredString("red", Color.Red),
+        ColoredString("yellow", Color.Yellow),
+        "default2",
+      )
+      val line2 = List[Message](
+        ColoredString("blue", Color.Blue),
+        ColoredString("green", Color.Green),
+        "default3",
+      )
+      println("visual test:")
+      display(List(line1, line2))
+      println("end visual test")
+      // This is a visual test.  You must look at the test output.
+    }
+  }
